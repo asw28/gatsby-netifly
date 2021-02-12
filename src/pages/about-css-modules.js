757910d190f7e7e3240c80513cbfe382c0b8f03b
aschw1 <as@biotx.ai>
2021-02-12 16:38:19 +0100

    Initial commit

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,34 @@
+import React from "react"
+import styles from "./about-css-modules.module.css"
+import Container from "../components/container"
+
+console.log(styles)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} className={styles.avatar} alt="" />
+    <div className={styles.description}>
+      <h2 className={styles.username}>{props.username}</h2>
+      <p className={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
+
+export default function About() {
+  return (
+    <Container>
+      <h1>What do you think about Jas?</h1>
+      <p>We asked several people what they think about Jas Remmers. Here is what they said.</p>
+      <User
+        username="Maria Randall"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-daniel-xavier-1102341.jpg"
+        excerpt="I'm Maria Randall. My opinion of Jas is that I think she is super beautiful. I would love to be like her."
+      />
+      <User
+        username="Daniela Dewitt"
+        avatar="https://raw.githubusercontent.com/gatsbyjs/gatsby/master/docs/docs/tutorial/part-two/pexels-guilherme-almeida-1858175.jpg"
+        excerpt="I'm Daniela Dewitt. I also think Jas is amazing. I regularly get envious thinking about her great charisma!"
+      />
+    </Container>
+  )
+}
